10a9243532e0443423f3203572564d60db6c77dc
LeozinH1 <lenardosj98@hotmail.com>
2020-11-04 03:43:51 -0300

    Set turma active in navbar

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -18,11 +18,11 @@
             <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
                 <a class="navbar-brand" href="#">AGENDE</a>
                 <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="">Home <span class="sr-only">(current)</span></a>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Home</a>
                     </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="">Minhas turmas</a>
+                        <li class="nav-item active">
+                        <a class="nav-link" href="">Turmas <span class="sr-only">(current)</span></a>
                     </li>
                         <li class="nav-item">
                         <a class="nav-link" href="">Tarefas</a>
